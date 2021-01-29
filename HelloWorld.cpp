8af5eebdd1d87bd5a3b372b99dc9965cbb32ad5a
Kevin Clause <kevin.clause@yahoo.com>
2021-01-28 20:10:41 -0500

    Added 'author' name

--- HelloWorld.cpp
+++ HelloWorld.cpp
@@ -1,5 +1,5 @@
 // Simple C++ program to display "Hello World" 
-// Header file for input output functions 
+// Author: Kevin Clause <go8212@wayne.edu>
 #include<iostream>  
   
 using namespace std; 
